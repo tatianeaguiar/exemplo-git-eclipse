723740ef5b2158d735eaced09266d04e9bdc364c
Tatiane Aguiar <tatiane.aguiar40@gmail.com>
2018-09-28 19:37:00 -0300

    commit inicial teste

---
+++ Curso Java/src/fundamentos/Comentário.java
@@ -0,0 +1,28 @@
+package fundamentos;
+
+	/**
+	 * Responsavel por mostrar tres tipos de comentários que o java suporta.
+	 * @author Tatiane Aguiar [tatiane.aguiar40@gmail.com]
+	 * @version 1.0
+	 * @since 2.2
+	 */
+
+public class Comentário {
+	
+		/**
+		 * Método de entrada para um programa java.
+		 * @param args parametros passados por linha de comando
+		 */
+	
+    public static void main(String[] args) {
+    	
+    	/*
+    	 * Imprime no console a palavra "Primeiro" e 
+    	 * em seguida cria uma nova linha
+    	 */
+    	
+		System.out.println("Primeiro");
+		// Cuidado com os comentários óbvios
+		System.out.println("Programa!");
+	}
+}
